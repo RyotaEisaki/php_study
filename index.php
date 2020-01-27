855d964113297cff45a2f12ac5ce0d2cf11ce2fe
MacBookPro <ryotaeisaki0530@icloud.com>
2020-01-28 00:43:38 +0900

    7

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <?php
-echo "hello from the TOP!";
-echo "hello from the TOP! again!";
-$msg = "hello from the TOP!";
-echo $msg;
-var_dump($msg);
-?>
-<!DOCTYPE html>
-<html lang="ja">
-<body>
-    <p>Hello World<?php echo " from PHP"; ?></p>
-</body>
-</html>
+// echo "hello from the TOP!";
+// echo "hello from the TOP! again!";
+// $msg = "hello from the TOP!";
+// echo $msg;
+// var_dump($msg);
+
+// $x=10%3;
+// $y=30.2/4;
+// var_dump($x);
+// var_dump($y);
+
+// $z=5;
+// $z++;
+// var_dump($z);
+// $z--;
+// var_dump($z);
+
+$x=5;
+$x*=2;
+var_dump($x);
+
+
+/* ?>
+// <!DOCTYPE html>
+// <html lang="ja">
+// <body>
+//     <p>Hello World<?php echo " from PHP"; ?></p>
+// </body>
+// </html>
+*/
---
+++ index2.php
@@ -0,0 +1,9 @@
+<?php
+
+define("MY_EMAIL","ryotaeisaki0530@icloud.com");
+
+echo MY_EMAIL;
+
+var_dump(__LINE__);
+var_dump(__FILE__);
+var_dump(__DIR__);

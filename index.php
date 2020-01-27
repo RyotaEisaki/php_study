e5fd2fe7f2bf9aa3da0c35e24dd45bb83546e61e
Ryota Eisaki <ryotaeisaki0530@icloud.com>
2020-01-28 00:25:26 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+echo "hello from the TOP!";
+echo "hello from the TOP! again!";
+$msg = "hello from the TOP!";
+echo $msg;
+var_dump($msg);
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<body>
+    <p>Hello World<?php echo " from PHP"; ?></p>
+</body>
+</html>
